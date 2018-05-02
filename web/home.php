a8912a977a5e6b8728a3729e634aceb732ccd4e2
Webb-Avery <averymwebb@gmail.com>
2018-05-02 14:02:12 -0600

    added home.php, deleted home.html, updated assignments.html

---
+++ web/home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <link rel="stylesheet" href="main.css">
+    <title>Home Page</title>
+</head>
+
+<body>
+    <header id="top"> 
+        <h1> Welcome to Avery Webb's Home Page</h1>
+        <hr>
+    </header>
+    
+  
+    <hr>
+    <footer>
+        
+        <div> 
+            <blockquote cite = "http://www.goodreads.com/quotes/2448-to-live-is-the-" style= 
+                        "font-family: Georgia; text-align: center; padding-bottom: 10px;">
+                <span style = "font-family: cursive; font-size: 20px; color: #E92594">
+                    “To live is the rarest thing in the world. Most people just exist.”</span>
+                <b>Oscar Wilde</b>
+            </blockquote>
+        </div>
+    
+        <p style= "text-align: center; font-family: cursive"> <a href="index.html">Assignments
+            </a>  |  <a href = "#top">Top of Page</a></p>
+    </footer>
+</body>
+</html>
