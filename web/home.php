fcbd877e1183b9fdec1e80b21a239c419e7284d0
Webb-Avery <averymwebb@gmail.com>
2018-05-03 18:56:34 -0600

    updated home

--- web/home.php
+++ web/home.php
@@ -15,12 +15,22 @@
   
   <p>My family has a wonderful dog named Coco (on Right), she is half toy poodle and half pug. This year our family decided it would be a fun adventure to have puppies! Bennie (on left) is a dog that lives down the street and the dad to our puppies. </p>
   <img src="pictures/mom_dad.jpg" alt="Mom and Dad dogs">
+
+  <p>Coco Bean got so big that she barely fit in her sweater!</p>
   <img src="pictures/pregnant_coco.jpg" alt="puppies">
+  <p>Then one day at 5 in the morning she had six puppies. We were hoping for four puppies, so we were amazed that six puppies could fit inside her! </p>
   <img src="pictures/tiny_puppies.jpg" alt="puppies">
-  <img src="pictures/tiny_puppy.jpg" alt="puppies">
+  
+  <p>The puppies were so small they could fit in one hand. </p>
   <img src="pictures/tiniest.jpg" alt="puppies">
+
+  <p>Then they grew so you had to use two hands to hold them</p>
+  <img src="pictures/tiny_puppy.jpg" alt="puppies">
+  <p>They loved to cuddle with each other. </p>
   <img src="pictures/puppy.jpg" alt="puppies">
+  <p>When they started to eat puppy food, they loved to sit inside there food dish</p>
   <img src="pictures/food_dish.jpg" alt="puppies">
+  <p>Eventually they were big enough to play with toys! This is the first toy they ever played with. </p>
   <img src="pictures/first_toy.jpg" alt="puppies">
 
 
