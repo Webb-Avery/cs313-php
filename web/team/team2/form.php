97c138ba54e90f30fa9de52fab47dafe63c96559
Webb-Avery <averymwebb@gmail.com>
2018-05-08 15:54:24 -0600

    added team 2

---
+++ web/team/team2/form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Form</title>
+        <link href="style.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <form action="" method="post">
+            
+            <p>Name:<input type="text"></p>
+            <p>Email:<input type="text"></p>
+
+            <p>Major:</p>
+            <ul> 
+                <li><input type="radio" name="major"></li>
+                <li><input type="radio" name="major"></li>
+                <li><input type="radio" name="major"></li>
+                <li><input type="radio" name="major"></li>
+            </ul>
+
+            <p>Comment: <textarea ></textarea></p>
+
+        </form>
+    </body>
+</html>
+
+    
