bb533c0ce8854946f50f9982ee8f2a292934db7c
Webb-Avery <averymwebb@gmail.com>
2018-05-10 14:42:35 -0600

    added shopping cart

---
+++ web/assign03/nav.php
@@ -0,0 +1,11 @@
+<header>
+    <h1>The Berry Shop!</h1>
+    
+</header>
+<nav>
+    
+    
+    <li><a href="items.php">Browse Items for Sale</a></li>
+    <li><a href="cart.php">View Cart</a></li>
+    <li><a href="home.php">Avery Webb Home Page</a></li>
+</nav>
