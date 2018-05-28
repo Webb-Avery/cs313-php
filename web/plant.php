60b33ebe6879bc218168e697bbe3684fa573adf7
Avery Webb <averymwebb@gmail.com>
2018-05-28 10:28:51 -0600

    added plants.php

--- web/plant.php
+++ web/plant.php
@@ -35,24 +35,23 @@ catch (PDOException $ex)
 
 <?php
 $sun = $_GET["sun"];
-echo "<h1> $sun </h1>";
 if($sun != 'none')
 {
     if($sun == 'fullsun')
     {
-        echo "<h1> FULL SUN </h1>";
+        echo "<h1>Plants needing full sun: </h1>";
         $sun = 'Full Sun';
 
     }
     else if($sun == 'partsun')
     {
-        echo "<h1> Part SUN </h1>";
+        echo "<h1>Plants needing part sun: </h1>";
         $sun = 'Part Sun';
 
     }
     else if($sun == 'fullshade')
     {
-        echo "<h1> FULL SHADE </h1>";
+        echo "<h1>Plants needing full shade: </h1>";
         $sun = 'Full Shade';
 
     }
@@ -100,6 +99,80 @@ if($sun != 'none')
 }
 ?>
 </table>
+
+
+<?php
+$water = $_GET["water"];
+if($water != 'none')
+{
+    if($water == '1')
+    {
+        echo "<h1>Plants needing 1 inch of water: </h1>";
+        $water = '1 inch';
+
+    }
+    else if($water == '2')
+    {
+        echo "<h1>Plants needing 2 inches of water: </h1>";
+        $water = '2 inches AND waterinches = 1-2 inches ';
+
+    }
+    else if($water == '3')
+    {
+        echo "<h1>Plants needing 3 inches of water: </h1>";
+        $water = '3 inches';
+
+    }
+    else 
+    {
+        echo "<h1> Error </h1>";
+    }
+
+    echo "<table style='width:100%'><tr>
+        <th>NAME </th>
+        <th>Sun exposure </th>
+        <th>Water needed per week in inches </th>
+        <th>When to plant </th>
+        <th>Plant Spread </th><th>Plant height </th>
+        <th>Life Cycle </th>
+        <th> Plant Type </th></tr>";
+
+    $query = "SELECT name, sunexposure, waterinches, timetoplant, height, spread, lifecycle, planttype FROM plants WHERE waterinches = :water";
+    $statement = $db->prepare($query);
+    $statement->bindValue(":water", $water, PDO::PARAM_STR);
+    $statement->execute();
+
+    foreach ($statement->fetchAll(PDO::FETCH_ASSOC) as $plant)
+    {
+
+        $name = $plant["name"];
+        $sun = $plant["sunexposure"];
+        $water = $plant["waterinches"];
+        $timeToPlant = $plant["timetoplant"];
+        $spread = $plant["spread"];
+        $height = $plant["height"];
+        $lifeCycle = $plant["lifecycle"];
+        $type = $plant["planttype"];
+        echo"<tr>";
+        echo "<td>$name</td>";
+        echo "<td>$sun</td>";
+        echo "<td>$water</td>";
+        echo "<td>$timeToPlant</td>";
+        echo "<td>$spread</td>";
+        echo "<td>$height</td>";
+        echo "<td>$lifeCycle</td>";
+        echo "<td>$type</td>";
+        echo "</tr>";
+    }
+}
+?>
+</table>
+
+
+
+
+
+
 
 <h1> The Entire Database, for testing </h1>
 <table>
