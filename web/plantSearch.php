9ee5bd1d0514739ad16612993f731c52efb7311f
Avery Webb <averymwebb@gmail.com>
2018-05-28 09:43:09 -0600

    added plants.php

--- web/plantSearch.php
+++ web/plantSearch.php
@@ -15,9 +15,9 @@
         <form action="plant.php" method="get">
             
             Sun Exposure <select>
-                <option value="FullSun" name="FullSun">Full Sun </option>
-                <option value="PartSun" name="PartSun">Part Sun</option>
-                <option value="FullShade" name="FullShade">Full Shade</option>
+                <option value="FullSun">Full Sun </option>
+                <option value="PartSun" >Part Sun</option>
+                <option value="FullShade" >Full Shade</option>
             </select>
 
             <input type="submit">
