22de9966a1143cb31fdeafdaa303aa95a0a6b4e8
Avery Webb <averymwebb@gmail.com>
2018-05-30 14:26:19 -0600

    added login

--- web/garden.php
+++ web/garden.php
@@ -16,17 +16,39 @@ session_start();
     </header>
     <body>
 
+        <h2>Login</h2>
+        <form method="post" action="">
+        
+            <label> Username: </label>
+            <input type="text" id="username" name="username"></input>
+            <br />
+            
+            <label> password: </label>
+            <input type="text" id="password" name="password"></input>
+            <br />
 
-            <main>
-                <section>
-                    <h2>Login</h2>
-                    <form method="post" action="">
-                        <input type="submit" value="Login!">
-                    </form>
-                    <form method="post" action="">
-                        <input type="submit" value="Sign Up!">
-                    </form>
-                </section>
-            </main>
+            <input type="submit" value="Login!">
+        </form>
+
+        <h2> Sign Up! </h2>
+        <form method="post" action="">
+            <label> First Name: </label>
+            <input type="text" id="firstName" name="firstName"></input>
+            <br />
+
+            <label> Last Name: </label>
+            <input type="text" id="lastName" name="lastName"></input>
+            <br />
+
+            <label> Username: </label>
+            <input type="text" id="username" name="username"></input>
+            <br />
+            
+            <label> password: </label>
+            <input type="text" id="password" name="password"></input>
+            <br />
+
+            <input type="submit" value="Sign Up!">
+        </form>
     </body>
 </html>
