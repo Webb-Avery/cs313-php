620110d1e8220e836ae78f09bc2674b0f61b3102
Avery Webb <averymwebb@gmail.com>
2018-05-30 14:57:18 -0600

    added login

--- web/myGarden.php
+++ web/myGarden.php
@@ -29,7 +29,7 @@ if(type == "signup") {
     {
         // Add the Scripture
         // We do this by preparing the query with placeholder values
-        $query = 'INSERT INTO users(firstname, lastname, username, password) VALUES(:firstname :lastname, :username, :password)';
+        $query = 'INSERT INTO users(firstname, lastname, username, 'password') VALUES(:firstname :lastname, :username, :password)';
         $statement = $db->prepare($query);
         // Now we bind the values to the placeholders. This does some nice things
         // including sanitizing the input with regard to sql commands.
