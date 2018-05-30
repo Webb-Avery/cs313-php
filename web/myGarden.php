528e9af10ffe44af24c9197f39682d4d7a7e3ead
Avery Webb <averymwebb@gmail.com>
2018-05-30 15:21:48 -0600

    added login

--- web/myGarden.php
+++ web/myGarden.php
@@ -22,57 +22,57 @@ catch (PDOException $ex)
 
 <!DOCTYPE html>
 <html>
-<head>
-<link rel="stylesheet" href="main.css">
-    <title>Plants</title>
-</head>
+    <head>
+    <link rel="stylesheet" href="main.css">
+        <title>Plants</title>
+    </head>
 
-<body>
-    <header id="top"> 
-    <?php include 'nav.php'; ?>
-    </header>
+    <body>
+        <header id="top"> 
+            <?php include 'nav.php'; ?>
+        </header>
 
+            
+        <?php
 
-<?php
-try
-{
+        $type = $_POST['hidden'];
+        echo "<p>$type</p>";
+        if(type == 'signup') 
+        {
+            $firstname = $_POST['firstname'];
+            $lasttname = $_POST['lastname'];
+            $username = $_POST['username'];
+            $password = $_POST['password'];
 
-$type = $_POST['hidden'];
-echo "<p>$type</p>";
-if(type == 'signup') {
-    $firstname = $_POST['firstname'];
-    $lasttname = $_POST['lastname'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-  /*  
-    try
-    {
-        // Add the Scripture
-        // We do this by preparing the query with placeholder values
-        $query = 'INSERT INTO users(firstname, lastname, username, password) VALUES(:firstname :lastname, :username, :password)';
-        $statement = $db->prepare($query); 
-        // Now we bind the values to the placeholders. This does some nice things
-        // including sanitizing the input with regard to sql commands.
-        $statement->bindValue(':firstname', $firstname);
-        $statement->bindValue(':lastname', $lastname);
-        $statement->bindValue(':username', $username);
-        $statement->bindValue(':password', $password);
-        $statement->execute();
-
-        echo "<p>'Successfull'</p>";
+            try
+            {
+                // Add the Scripture
+                // We do this by preparing the query with placeholder values
+                $query = 'INSERT INTO users(firstname, lastname, username, password) VALUES(:firstname :lastname, :username, :password)';
+                $statement = $db->prepare($query);
+                // Now we bind the values to the placeholders. This does some nice things
+                // including sanitizing the input with regard to sql commands.
+                $statement->bindValue(':firstname', $firstname);
+                $statement->bindValue(':lastname', $lastname);
+                $statement->bindValue(':username', $username);
+                $statement->bindValue(':password', $password);
+                $statement->execute();
+         
+                echo "<p>'Successfull'</p>";
+           
+            }
+            catch (Exception $ex)
+            {
+                // Please be aware that you don't want to output the Exception message in
+                // a production environment
+                echo "Error with DB. Details: $ex";
+                die();
+            }
+         
+            
+        }
     
-    }
-    catch (Exception $ex)
-    {
-        // Please be aware that you don't want to output the Exception message in
-        // a production environment
-        echo "Error with DB. Details: $ex";
-        die();
-    }
-    */
-}
-?>
-
-</body>
+        ?>
+        
+    </body>
 </html>
