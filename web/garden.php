5ab9af1197b4e88b0ec37a89cdae09d8614968e1
Avery Webb <averymwebb@gmail.com>
2018-05-30 14:14:04 -0600

    added login

---
+++ web/garden.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="main.css">
+    <title>Plants</title>
+</head>
+
+<body>
+    <header id="top"> 
+    <?php include 'nav.php'; ?>
+    </header>
+    <body>
+
+
+            <main>
+                <section>
+                    <h2>Login</h2>
+                    <form method="post" action="">
+                        <input type="submit" value="Login!">
+                    </form>
+                    <form method="post" action="">
+                        <input type="submit" value="Sign Up!">
+                    </form>
+                </section>
+            </main>
+    </body>
+</html>
