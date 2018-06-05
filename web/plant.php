3dde81303116c5dc1c594169200ebe13f4cd5276
Avery Webb <averymwebb@gmail.com>
2018-06-05 10:30:57 -0600

    change password

--- web/plant.php
+++ web/plant.php
@@ -42,7 +42,7 @@ catch (PDOException $ex)
 
     echo "<h1>Plants perfect for your zone!</h1>";
     
-    echo "<table style='width:100%'><tr>
+    echo "<table style='width:80%'><tr>
         <th>NAME </th>
         <th>Sun exposure </th>
         <th>Water needed per week in inches </th>
@@ -173,7 +173,7 @@ catch (PDOException $ex)
 
 
 <h1> Browse All Plants (These might not be perfect for your zones) </h1>
-<table>
+<table style='width:80%'>
     <tr>
         <th>NAME </th>
         <th>Sun exposure </th>
