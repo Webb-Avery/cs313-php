16b627b75c3f70f72c711ba35905e012330ad084
Avery Webb <averymwebb@gmail.com>
2018-06-11 21:52:45 -0600

    fixing things

--- web/garden.php
+++ web/garden.php
@@ -25,7 +25,7 @@ if(!isset($_SESSION['usernameError'])){
     <?php include 'nav.php'; ?>
     </header>
     <body>
-
+<br>
         
         <form method="post" action="myGarden.php">
              <h2>Login</h2>
